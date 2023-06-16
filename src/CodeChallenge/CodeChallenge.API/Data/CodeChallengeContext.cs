95f04af9d29293a817d6b06b9333300c2e9b1454
filipe <filipe@s1data.com>
2023-06-16 08:41:17 -0300

    Creating project

---
+++ src/CodeChallenge/CodeChallenge.API/Data/CodeChallengeContext.cs
@@ -0,0 +1,13 @@
+﻿using CodeChallenge.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeChallenge.API.Data
+{
+    public class CodeChallengeContext : DbContext
+    {
+        public CodeChallengeContext(DbContextOptions<CodeChallengeContext> options)
+            : base(options)
+        { }
+            public DbSet<Customer> Customers { get; set; }
+    }
+}
